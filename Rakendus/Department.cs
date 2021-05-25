120f504b1c0aaf2ca83ad5278ca3c03bf6223eb2
Kätlin Linder <klinder@tmk.edu.ee>
2021-05-25 13:44:16 +0300

    esimene video

---
+++ Rakendus/Department.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Rakendus
+{
+    public class Department
+    {
+        public int EmployeeId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
