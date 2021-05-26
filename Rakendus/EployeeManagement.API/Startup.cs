85a2214eaabfdc866ab90047bd7bdcaae7118aa8
Kätlin Linder <klinder@tmk.edu.ee>
2021-05-26 12:42:21 +0300

    video 12

--- Rakendus/EployeeManagement.API/Startup.cs
+++ Rakendus/EployeeManagement.API/Startup.cs
@@ -2,12 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EployeeManagement.API.Models2;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
 
 namespace EployeeManagement.API
 {
@@ -23,7 +29,10 @@ namespace EployeeManagement.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
