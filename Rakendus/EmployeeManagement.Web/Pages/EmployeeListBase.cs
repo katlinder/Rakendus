70a8da1f52ece8a9db0673fbc5a7e25c6d2e10f6
Kätlin Linder <klinder@tmk.edu.ee>
2021-05-26 10:44:24 +0300

    video 10

--- Rakendus/EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ Rakendus/EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -28,7 +28,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "David@pragimtech.com",
                 DateOfBrith = new DateTime(1980, 10, 5),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/john.png"
             };
 
@@ -40,7 +40,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "Sam@pragimtech.com",
                 DateOfBrith = new DateTime(1981, 12, 22),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                DepartmentId = 2,
                 PhotoPath = "images/sam.jpg"
             };
 
@@ -52,7 +52,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "mary@pragimtech.com",
                 DateOfBrith = new DateTime(1979, 11, 11),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/mary.png"
             };
 
@@ -64,7 +64,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "sara@pragimtech.com",
                 DateOfBrith = new DateTime(1982, 9, 23),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                DepartmentId = 3,
                 PhotoPath = "images/sara.png"
             };
 
