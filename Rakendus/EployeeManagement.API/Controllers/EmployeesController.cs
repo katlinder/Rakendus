1f6d6ee45ab0fd3536d473d2fe0afc0b9cb25134
Kätlin Linder <klinder@tmk.edu.ee>
2021-05-27 13:03:15 +0300

    video 16

--- Rakendus/EployeeManagement.API/Controllers/EmployeesController.cs
+++ Rakendus/EployeeManagement.API/Controllers/EmployeesController.cs
@@ -3,8 +3,6 @@ using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Api.Controllers
@@ -21,11 +19,11 @@ namespace EmployeeManagement.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        public async Task<ActionResult> GetEmployees()
         {
             try
             {
-                return (await employeeRepository.GetEmployees()).ToList();
+                return Ok(await employeeRepository.GetEmployees());
             }
             catch (Exception)
             {
@@ -41,7 +39,10 @@ namespace EmployeeManagement.Api.Controllers
             {
                 var result = await employeeRepository.GetEmployee(id);
 
-                if (result == null) return NotFound();
+                if (result == null)
+                {
+                    return NotFound();
+                }
 
                 return result;
             }
@@ -51,24 +52,26 @@ namespace EmployeeManagement.Api.Controllers
                     "Error retrieving data from the database");
             }
         }
-      
-            public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>>
+            CreateEmployee([FromBody] Employee employee)
+        {
+            try
             {
-                try
-                {
-                    if (employee == null)
-                        return BadRequest();
+                if (employee == null)
+                    return BadRequest();
 
-                    var createdEmployee = await employeeRepository.AddEmployee(employee);
+                var createdEmployee = await employeeRepository.AddEmployee(employee);
 
-                    return CreatedAtAction(nameof(GetEmployee),
-                        new { id = createdEmployee.EmployeeId }, createdEmployee);
-                }
-                catch (Exception)
-                {
-                    return StatusCode(StatusCodes.Status500InternalServerError,
-                        "Error creating new employee record");
-                }
+                return CreatedAtAction(nameof(GetEmployee),
+                    new { id = createdEmployee.EmployeeId }, createdEmployee);
             }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error creating new employee record");
+            }
+        }
     }
 }
