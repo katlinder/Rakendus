c44d1da64891dbc4e777670997892c3538b53da5
Kätlin Linder <klinder@tmk.edu.ee>
2021-05-27 12:34:47 +0300

    video 14

--- Rakendus/EployeeManagement.API/Controllers/EmployeesController.cs
+++ Rakendus/EployeeManagement.API/Controllers/EmployeesController.cs
@@ -1,11 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using EmployeeManagement.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Api.Controllers
 {
-    public class EmployeesController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
     {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
     }
 }
