613d67158b0253e5fe5b3433423f3b7113efba8f
Kätlin Linder <klinder@tmk.edu.ee>
2021-06-01 09:56:38 +0300

    26

--- Rakendus/EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
+++ Rakendus/EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -8,10 +8,6 @@ namespace EmployeeManagement.Web.Pages
 {
     public class DatabindingDemoBase : ComponentBase
     {
-        protected string Name { get; set; } = "Tom";
-
-        protected string Gender { get; set; } = "Male";
-
-        protected string Colour { get; set; } = "background-color:white";
+        public string Description { get; set; } = string.Empty;
     }
 }
