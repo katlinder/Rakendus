69bcf4edef2f10fa4f805bdb9ab2c26427a5e888
Kätlin Linder <klinder@tmk.edu.ee>
2021-06-11 13:36:34 +0300

    56

--- Rakendus/EmployeeManagement.Web/Startup.cs
+++ Rakendus/EmployeeManagement.Web/Startup.cs
@@ -31,7 +31,7 @@ namespace EmployeeManagement.Web
         {
                 services.AddAuthentication("Identity.Application")
                     .AddCookie();
-                services.AddRazorPages();
+            services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddAutoMapper(typeof(EmployeeProfile));
             services.AddHttpClient<IEmployeeService, EmployeeService>(client =>
@@ -63,10 +63,9 @@ namespace EmployeeManagement.Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseRouting();           
-            app.UseAuthorization();
-            
-
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();  
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
